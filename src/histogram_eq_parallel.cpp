ffb4a10874a29700c94f7159676450faca1c0be2
goncalovirginia <gonvirginia.blueslime@gmail.com>
2024-05-30 02:50:48 +0100

    -300ms from loop optimizations

--- src/histogram_eq_parallel.cpp
+++ src/histogram_eq_parallel.cpp
@@ -12,49 +12,57 @@ namespace cp {
 		return (float) x / (float) size;
 	}
 
-	static unsigned char inline clamp(unsigned char x) {
-		return std::min(std::max(x, static_cast<unsigned char>(0)), static_cast<unsigned char>(255));
+	static float inline clamp(float x) {
+		return std::min(std::max(x, static_cast<float>(0.0)), static_cast<float>(1.0));
 	}
 
-	static unsigned char inline correct_color(float cdf_val, float cdf_min) {
-		return clamp(static_cast<unsigned char>(255 * (cdf_val - cdf_min) / (1 - cdf_min)));
+	static float inline correct_color(float cdf_val, float cdf_min) {
+		return clamp((cdf_val - cdf_min) / (1 - cdf_min));
 	}
 
 	static void percentageTo255(const float *input_image_data, const std::shared_ptr<unsigned char[]> &uchar_image,
 								int size_channels) {
 		#pragma omp parallel for
-		for (int i = 0; i < size_channels; i++)
+		for (int i = 0; i < size_channels; ++i)
 			uchar_image[i] = (unsigned char) (255 * input_image_data[i]);
 	}
 
 	static void grayScale(int height, int width, const std::shared_ptr<unsigned char[]> &uchar_image,
 						  const std::shared_ptr<unsigned char[]> &gray_image) {
 		#pragma omp parallel for
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				auto idx = i * width + j;
-				auto r = uchar_image[3 * idx];
-				auto g = uchar_image[3 * idx + 1];
-				auto b = uchar_image[3 * idx + 2];
+		for (int i = 0; i < height; ++i) {
+			for (int j = 0; j < width; ++j) {
+				int idx = i * width + j;
+				unsigned char r = uchar_image[3 * idx];
+				unsigned char g = uchar_image[3 * idx + 1];
+				unsigned char b = uchar_image[3 * idx + 2];
 				gray_image[idx] = static_cast<unsigned char>(0.21 * r + 0.71 * g + 0.07 * b);
 			}
 		}
 	}
 
-	static void computeHistogram(const int size, int (&histogram)[HISTOGRAM_LENGTH],
-								 const std::shared_ptr<unsigned char[]> &gray_image) {
-		#pragma omp parallel for
-		for (int i = 0; i < size; i++)
-			histogram[gray_image[i]]++;
+	static void computeHistogram(const int size, int (&histogram)[HISTOGRAM_LENGTH], const std::shared_ptr<unsigned char[]> &gray_image) {
+		std::fill(histogram, histogram + HISTOGRAM_LENGTH, 0);
+
+		#pragma omp parallel for reduction(+:histogram)
+		for (int i = 0; i < size; ++i) {
+			++histogram[gray_image[i]];
+		}
 	}
 
 	static float computeCDF(float (&cdf)[HISTOGRAM_LENGTH], int (&histogram)[HISTOGRAM_LENGTH], const int size) {
-		cdf[0] = prob(histogram[0], size);
+		#pragma omp parallel for schedule(static, 64)
+		for (int i = 0; i < HISTOGRAM_LENGTH; ++i) {
+			cdf[i] = prob(histogram[i], size);
+		}
+
 		float cdf_min = cdf[0];
-		for (int i = 1; i < HISTOGRAM_LENGTH; i++) {
-			cdf[i] = cdf[i - 1] + prob(histogram[i], size);
+
+		for (int i = 1; i < HISTOGRAM_LENGTH; ++i) {
+			cdf[i] += cdf[i-1];
 			cdf_min = std::min(cdf_min, cdf[i]);
 		}
+
 		return cdf_min;
 	}
 
@@ -62,12 +70,12 @@ namespace cp {
 								   const std::shared_ptr<unsigned char[]> &uchar_image, float cdf_min,
 								   int size_channels) {
 		#pragma omp parallel for
-		for (int i = 0; i < size_channels; i++) {
-			output_image_data[i] = static_cast<float>(correct_color(cdf[uchar_image[i]], cdf_min)) / 255.0f;
+		for (int i = 0; i < size_channels; ++i) {
+			output_image_data[i] = correct_color(cdf[uchar_image[i]], cdf_min);
 		}
 	}
 
-	static void histogram_equalization(const int width, const int height,
+	static void histogram_equalization(const int width, const int height, const int size, const int size_channels,
 									   const float *input_image_data,
 									   float *output_image_data,
 									   const std::shared_ptr<unsigned char[]> &uchar_image,
@@ -75,15 +83,9 @@ namespace cp {
 									   int (&histogram)[HISTOGRAM_LENGTH],
 									   float (&cdf)[HISTOGRAM_LENGTH]) {
 
-		constexpr auto channels = 3;
-		const int size = width * height;
-		const int size_channels = size * channels;
-
 		percentageTo255(input_image_data, uchar_image, size_channels);
 
 		grayScale(height, width, uchar_image, gray_image);
-
-		std::fill(histogram, histogram + HISTOGRAM_LENGTH, 0);
 
 		computeHistogram(size, histogram, gray_image);
 
@@ -93,11 +95,11 @@ namespace cp {
 	}
 
 	wbImage_t iterative_histogram_equalization(wbImage_t &input_image, int iterations) {
-		const auto width = wbImage_getWidth(input_image);
-		const auto height = wbImage_getHeight(input_image);
-		constexpr auto channels = 3;
-		const auto size = width * height;
-		const auto size_channels = size * channels;
+		const int width = wbImage_getWidth(input_image);
+		const int height = wbImage_getHeight(input_image);
+		const int channels = 3;
+		const int size = width * height;
+		const int size_channels = size * channels;
 
 		wbImage_t output_image = wbImage_new(width, height, channels);
 		float *input_image_data = wbImage_getData(input_image);
@@ -109,8 +111,8 @@ namespace cp {
 		int histogram[HISTOGRAM_LENGTH];
 		float cdf[HISTOGRAM_LENGTH];
 
-		for (int i = 0; i < iterations; i++) {
-			histogram_equalization(width, height,
+		for (int i = 0; i < iterations; ++i) {
+			histogram_equalization(width, height, size, size_channels,
 								   input_image_data, output_image_data,
 								   uchar_image, gray_image,
 								   histogram, cdf);
